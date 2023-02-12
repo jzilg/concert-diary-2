b7d77686447d1633bf258a270360a6f9e50fd76b
jzilg <jzg@dasburo.com>
2023-02-12 22:11:53 +0100

    add error message if login fails

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react'
-import { Form } from '@remix-run/react'
+import { Form, useActionData } from '@remix-run/react'
 import type { ActionFunction, MetaFunction } from '@remix-run/node'
 import { extractStringFromBody } from '~/helpers/extractFromBody'
-import { redirect } from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
 import Input from '~/components/Input'
 import type LoginDto from '~/entities/LoginDto'
 import { getSession, commitSession } from '~/logic/session'
@@ -27,7 +27,9 @@ export const action: ActionFunction = async ({ request }) => {
   if (!userIsAuthenticated) {
     session.flash('error', 'Invalid username/password')
 
-    return redirect('/login', {
+    return json({
+      error: 'Wrong username or password',
+    }, {
       status: 401,
       headers: {
         'Set-Cookie': await commitSession(session),
@@ -44,43 +46,50 @@ export const action: ActionFunction = async ({ request }) => {
   })
 }
 
-const Login: FC = () => (
-  <div className="container mx-auto">
-    <h1 className="text-4xl font-bold my-6">Concert Diary</h1>
-    <Form method="post">
-      <h2 className="text-2xl font-bold">Login</h2>
-      <label className="block mt-3">
-        <span className="block mb-2 font-bold">Username</span>
-        <Input
-          type="text"
-          name="username"
-          minLength={2}
-          required
-        />
-      </label>
-      <label className="block mt-3">
-        <span className="block mb-2 font-bold">Password</span>
-        <Input
-          type="password"
-          name="password"
-          minLength={6}
-          required
-        />
-      </label>
-      <ul className="flex gap-2 mt-6">
-        <li>
-          <Button type="submit">
-            Login
-          </Button>
-        </li>
-        <li>
-          <NavLink to="/register">
-            Register
-          </NavLink>
-        </li>
-      </ul>
-    </Form>
-  </div>
-)
+const Login: FC = () => {
+  const actionData = useActionData()
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold my-6">Concert Diary</h1>
+      <Form method="post">
+        <h2 className="text-2xl font-bold">Login</h2>
+        <label className="block mt-3">
+          <span className="block mb-2 font-bold">Username</span>
+          <Input
+            type="text"
+            name="username"
+            minLength={2}
+            required
+          />
+        </label>
+        <label className="block mt-3">
+          <span className="block mb-2 font-bold">Password</span>
+          <Input
+            type="password"
+            name="password"
+            minLength={6}
+            required
+          />
+        </label>
+        <p className="mt-6 text-red-600">
+          {actionData?.error}
+        </p>
+        <ul className="flex gap-2 mt-6">
+          <li>
+            <Button type="submit">
+              Login
+            </Button>
+          </li>
+          <li>
+            <NavLink to="/register">
+              Register
+            </NavLink>
+          </li>
+        </ul>
+      </Form>
+    </div>
+  )
+}
 
 export default Login
