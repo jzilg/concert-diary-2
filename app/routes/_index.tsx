86296c29d760d74d85fa62b1a1318de30de4d033
Johannes Zilg <mail@jzilg.de>
2025-10-15 10:03:21 +0200

    remove unnecessary hook because redirecting in loader works without it

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,9 @@
 import type { FC } from 'react'
-import { redirect, useLoaderData } from 'react-router'
+import { redirect } from 'react-router'
 
 export const loader = () => redirect('/concerts')
 
 const Index: FC = () => {
-  useLoaderData()
-
   return undefined
 }
 
