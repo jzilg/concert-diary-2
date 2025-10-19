522d930ed29b98515ca780b03e63d31cb1e805bc
Johannes Zilg <mail@jzilg.de>
2025-10-19 21:14:48 +0200

    implicic over explcic typing and replace null with undefined

--- app/logic/user.ts
+++ app/logic/user.ts
@@ -1,15 +1,12 @@
 import bcrypt from 'bcryptjs'
 import uniqid from 'uniqid'
 import config from '~/config'
-import type { User } from '~/entities/User'
 import usersProvider from '~/providers/usersProvider'
 import { getSession } from './session'
 
 const { compare, genSalt, hash } = bcrypt
 
-export const getUserFromRequest = async (
-  request: Request,
-): Promise<User | undefined> => {
+export const getUserFromRequest = async (request: Request) => {
   const session = await getSession(request.headers.get('Cookie'))
   const userId = session.get('userId')
   const user = await usersProvider.getUserByField('id', userId)
@@ -17,27 +14,20 @@ export const getUserFromRequest = async (
   return user ?? undefined
 }
 
-export const authenticateUser = async (
-  username: string,
-  password: string,
-): Promise<[boolean, User | null]> => {
-  const user = await usersProvider.getUserByField('username', username)
+export const authenticateUser = async (username: string, password: string) => {
+  const user =
+    (await usersProvider.getUserByField('username', username)) ?? undefined
 
-  return [await compare(password, user?.password ?? ''), user]
+  return [await compare(password, user?.password ?? ''), user] as const
 }
 
 export const validateToken = (incomingToken: string): boolean =>
   incomingToken !== config.registerToken
 
-export const userAlreadyExists = async (
-  incomingUsername: string,
-): Promise<boolean> =>
+export const userAlreadyExists = async (incomingUsername: string) =>
   (await usersProvider.getUserByField('username', incomingUsername)) !== null
 
-export const createNewUser = async (
-  username: string,
-  password: string,
-): Promise<void> => {
+export const createNewUser = async (username: string, password: string) => {
   const salt = await genSalt()
   const encryptedPassword = await hash(password, salt)
 
