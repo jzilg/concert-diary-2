69f42c11b6063d57cadb231ea474260b070fba64
Johannes Zilg <mail@jzilg.de>
2026-03-28 13:39:36 +0100

    update tsconfig to improve code quality

--- app/config.ts
+++ app/config.ts
@@ -1,9 +1,11 @@
+/** biome-ignore-all lint/complexity/useLiteralKeys: conflict with ts compiler */
+
 export default Object.freeze({
-  modeIsDevelopment: process.env.NODE_ENV === 'development',
-  dbClient: process.env.DB_CLIENT || '',
-  dbAuthSource: process.env.DB_AUTH_SOURCE || '',
-  dbAuthUser: process.env.DB_AUTH_USER || '',
-  dbAuthPassword: process.env.DB_AUTH_PASSWORD || '',
-  sessionCookieSecret: process.env.SESSION_COOKIE_SECRET || '',
-  registerToken: process.env.REGISTER_TOKEN || '',
+  modeIsDevelopment: process.env['NODE_ENV'] === 'development',
+  dbClient: process.env['DB_CLIENT'] || '',
+  dbAuthSource: process.env['DB_AUTH_SOURCE'] || '',
+  dbAuthUser: process.env['DB_AUTH_USER'] || '',
+  dbAuthPassword: process.env['DB_AUTH_PASSWORD'] || '',
+  sessionCookieSecret: process.env['SESSION_COOKIE_SECRET'] || '',
+  registerToken: process.env['REGISTER_TOKEN'] || '',
 })
